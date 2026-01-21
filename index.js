768e48f283f122cfec604ff016070fb46a20db8e
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-01-21 19:28:15 +0500

    Update index.js and add index.html

--- index.js
+++ index.js
@@ -1,45 +1,83 @@
-const express = require('express');
-const server = express();
+// const express = require('express');
+// const server = express();
+// const port = 3000;
+
+// // General Middleware 1 (har request pe chalega)
+// server.use((req, res, next) => {
+//   console.log("Middleware 1:", req.method);
+//   next();
+// });
+
+// // General Middleware 2 (har request pe chalega)
+// server.use((req, res, next) => {
+//   console.log("Middleware 2:", req.path);
+//   next();
+// });
+
+// // Middleware specific for '/' route
+// server.use('/', (req, res, next) => {
+//   console.log("Middleware for / route");
+//   next(); // aage route handler ko allow karna
+// });
+
+// // Middleware specific for '/about' route
+// server.use('/about', (req, res, next) => {
+//   console.log("Middleware for /about route");
+//   next(); // aage route handler ko allow karna
+// });
+
+// // Route handlers
+// server.get('/', (req, res) => {
+//   res.send("Hello from /");
+// });
+
+// server.get('/about', (req, res) => {
+//   res.send("Hello from /about");
+// });
+
+// // Catch-all 3rd middleware (final response for unmatched routes)
+// server.use((req, res) => {
+//   res.send("3rd response is end here");
+// });
+
+// server.listen(port, () => {
+//   console.log("http://localhost:3000");
+// });
+
+const server = require("express");
+const app = server();
 const port = 3000;
 
-// General Middleware 1 (har request pe chalega)
-server.use((req, res, next) => {
-  console.log("Middleware 1:", req.method);
+app.use((req, res, next) => {
+  console.log("1: miiddleware" + req.url, req.method);
   next();
 });
 
-// General Middleware 2 (har request pe chalega)
-server.use((req, res, next) => {
-  console.log("Middleware 2:", req.path);
+app.use((req, res, next) => {
+  console.log("2: miiddleware" + req.url, req.method);
   next();
 });
 
-// Middleware specific for '/' route
-server.use('/', (req, res, next) => {
-  console.log("Middleware for / route");
-  next(); // aage route handler ko allow karna
+app.use("/contact-us", (req, res) => {
+  console.log("contact us " + req.url, req.method);
+  res.send(`
+    
+      <form action="contact-us" method="post">
+
+    <input type="text"  placeholder="name">
+    <input type="text" placeholder="email">
+    <input type="submit" value="Submit">
+      </form>
+    `);
 });
 
-// Middleware specific for '/about' route
-server.use('/about', (req, res, next) => {
-  console.log("Middleware for /about route");
-  next(); // aage route handler ko allow karna
+app.post('/contact-us', (req, res) => {
+  console.log("POST request received");
+  res.send("Form submitted successfully!");
 });
 
-// Route handlers
-server.get('/', (req, res) => {
-  res.send("Hello from /");
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
 });
 
-server.get('/about', (req, res) => {
-  res.send("Hello from /about");
-});
-
-// Catch-all 3rd middleware (final response for unmatched routes)
-server.use((req, res) => {
-  res.send("3rd response is end here");
-});
-
-server.listen(port, () => {
-  console.log("http://localhost:3000");
-});
+app.get("/", (req, res) => {});
