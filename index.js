0e75a3f16358db28a64c5d63f404cfe7f218a645
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-01-28 11:59:43 +0500

    today practeces code

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 import mongoose from "mongoose";
 import express from "express";
-import coursemodel from './models/coursemodel.js';  // ES Module style
+import coursemodel from "./models/coursemodel.js"; // ES Module style
 
 const app = express();
 app.use(express.json()); // optional, for POST requests
 
-app.get('/courses', async (req, res) => {
+app.get("/courses", async (req, res) => {
   try {
     const result = await coursemodel.find({});
     res.status(200).send({
       isSuccessful: true,
-      data: result
+      data: result,
     });
   } catch (error) {
     res.status(500).send({
       isSuccessful: false,
       message: error.message,
-      data: error
+      data: error,
+    });
+  }
+});
+
+app.post("/courses", (req, res) => {
+  try {
+     const body = req.body
+     const obj = {
+      name
+     }
+
+  } catch (error) {
+       res.status(500).send({
+      isSuccessful: false,
+      message: error.message,
+      data: error,
     });
   }
 });
@@ -25,7 +41,7 @@ const port = 3000;
 
 mongoose
   .connect(
-    "mongodb+srv://muhabbatali:myworldisstart%24%24%24@cluster0.fepov5t.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://muhabbatali:myworldisstart%24%24%24@cluster0.fepov5t.mongodb.net/test?retryWrites=true&w=majority",
   )
   .then(() => {
     console.log("MongoDB connect ho gaya");
